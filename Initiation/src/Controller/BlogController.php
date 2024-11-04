1284f088c3227809abc8c9af95a100536455fccf
Benkerri Ilyes <ilyes.benkerri27@gmail.com>
2024-11-04 21:21:53 +0100

    Partie 2

--- Initiation/src/Controller/BlogController.php
+++ Initiation/src/Controller/BlogController.php
@@ -7,11 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\ArticleType;
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="app_blog")
+     * @Route("/blog", name="blog")
      */
     public function index(ArticleRepository $repo): Response
     {   
@@ -29,6 +36,32 @@ class BlogController extends AbstractController
             'title' => "Bienvenue sur le blog",
             'age' => 31
         ]);
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function form(Article $article = null ,Request $request, EntityManagerInterface $manager){
+        if(!$article){
+            $article = new Article();
+        }
+        
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$article->getId()){
+                $article->setCreatedAt(new \DateTime());
+            }
+            $manager->persist($article);
+            $manager->flush();
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
+
     }
 
     /**
